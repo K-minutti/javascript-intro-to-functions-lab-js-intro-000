eb9eb2712584616af6be9abb9597048a3f2f2a7e
Kevin Minutti <minutti70@gmail.com>
2020-03-21 02:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function whisper(string) {
 
 
 var uppercase = "HELLO!"
- 
+
 uppercase.toUpperCase() === uppercase // true
- 
+
 var lowercase = 'hello!'
- 
+
 lowercase.toLowerCase() === lowercase // true
- 
+
 var mixedCase = 'Hi there!'
- 
+
 mixedCase.toLowerCase() === mixedCase // false
- 
+
 mixedCase.toUpperCase() === mixedCase // false
